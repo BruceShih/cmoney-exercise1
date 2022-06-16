da200a0bf3067a73f6335798058e51266659212d
Bruce Shih <sp1986@gmail.com>
2022-06-16 22:52:16 +0800

    mobile remain

--- js/index.js
+++ js/index.js
@@ -174,52 +174,99 @@
   function populateFoods(mode) {
     var data = getData(selectedCity, selectedTown, currentPage);
     var foods = document.getElementById('foods');
-    // foods.innerHTML = '';
+    foods.innerHTML = '';
 
     if (mode === 'table') {
       var foodTable = document.createElement('table');
-        foodTable.className =
-          'table table-striped table-hover table-bordered table-responsive food-table';
+      foodTable.className =
+        'table table-striped table-hover table-bordered table-responsive food-table';
 
-        var foodTableHead = document.createElement('thead');
-        var foodTableHeadRow = document.createElement('tr');
-        var headers = ['編號', '行政區域', '鄉鎮區', '商家', '地址'];
-        for (var i = 0; i < headers.length; i++) {
-          var foodTableHeadRowCell = document.createElement('th');
-          foodTableHeadRowCell.innerText = headers[i];
-          foodTableHeadRow.appendChild(foodTableHeadRowCell);
-        }
-        foodTableHead.appendChild(foodTableHeadRow);
-        foodTable.appendChild(foodTableHead);
+      var foodTableHead = document.createElement('thead');
+      var foodTableHeadRow = document.createElement('tr');
+      var headers = ['編號', '行政區域', '鄉鎮區', '商家', '地址'];
+      for (var i = 0; i < headers.length; i++) {
+        var foodTableHeadRowCell = document.createElement('th');
+        foodTableHeadRowCell.innerText = headers[i];
+        foodTableHeadRow.appendChild(foodTableHeadRowCell);
+      }
+      foodTableHead.appendChild(foodTableHeadRow);
+      foodTable.appendChild(foodTableHead);
 
-        var foodTableBody = document.createElement('tbody');
-        for (var i = 0; i < data.length; i++) {
-          var food = data[i];
-          var foodTableBodyRow = document.createElement('tr');
-          var foodTableBodyId = document.createElement('td');
-          foodTableBodyId.innerText = ((i + 1) + (currentPage - 1) * pageSize).toString();
-          foodTableBodyRow.appendChild(foodTableBodyId);
-          var foodTableBodyCity = document.createElement('td');
-          foodTableBodyCity.innerText = food.City;
-          foodTableBodyRow.appendChild(foodTableBodyCity);
-          var foodTableBodyTown = document.createElement('td');
-          foodTableBodyTown.innerText = food.Town;
-          foodTableBodyRow.appendChild(foodTableBodyTown);
-          var foodTableBodyName = document.createElement('td');
-          foodTableBodyName.innerText = food.Name;
-          foodTableBodyRow.appendChild(foodTableBodyName);
-          var foodTableBodyAddress = document.createElement('td');
-          foodTableBodyAddress.innerText = food.Address;
-          foodTableBodyRow.appendChild(foodTableBodyAddress);
-          foodTableBody.appendChild(foodTableBodyRow);
-        }
-        foodTable.appendChild(foodTableBody);
-        // foods.appendChild(foodTable);
+      var foodTableBody = document.createElement('tbody');
+      for (var i = 0; i < data.length; i++) {
+        var food = data[i];
+        var foodTableBodyRow = document.createElement('tr');
+        var foodTableBodyId = document.createElement('td');
+        foodTableBodyId.innerText = (
+          i +
+          1 +
+          (currentPage - 1) * pageSize
+        ).toString();
+        foodTableBodyRow.appendChild(foodTableBodyId);
+        var foodTableBodyCity = document.createElement('td');
+        foodTableBodyCity.innerText = food.City;
+        foodTableBodyRow.appendChild(foodTableBodyCity);
+        var foodTableBodyTown = document.createElement('td');
+        foodTableBodyTown.innerText = food.Town;
+        foodTableBodyRow.appendChild(foodTableBodyTown);
+        var foodTableBodyName = document.createElement('td');
+        foodTableBodyName.innerText = food.Name;
+        foodTableBodyRow.appendChild(foodTableBodyName);
+        var foodTableBodyAddress = document.createElement('td');
+        foodTableBodyAddress.innerText = food.Address;
+        foodTableBodyRow.appendChild(foodTableBodyAddress);
+        foodTableBody.appendChild(foodTableBodyRow);
+      }
+      foodTable.appendChild(foodTableBody);
+      foods.appendChild(foodTable);
+    } else if (mode === 'grid') {
+      for (var i = 0; i < data.length; i++) {
+        var food = data[i];
+        var foodItem = document.createElement('div');
+        foodItem.className = 'food food-grid';
+
+        var foodImage = document.createElement('div');
+        foodImage.className = 'food-img';
+        var img = document.createElement('img');
+        img.src = food.PicURL;
+        img.alt = food.Name;
+        img.loading = 'lazy';
+        foodImage.appendChild(img);
+        foodItem.appendChild(foodImage);
+
+        var foodContent = document.createElement('div');
+        foodContent.className = 'food-content';
+        var foodSubtitle = document.createElement('div');
+        foodSubtitle.className = 'food-subtitle';
+        var foodBadge = document.createElement('div');
+        foodBadge.className = 'badge';
+        foodBadge.innerText = food.City;
+        var foodCaption = document.createElement('div');
+        foodCaption.className = 'caption';
+        foodCaption.innerText = food.Town;
+        foodSubtitle.appendChild(foodBadge);
+        foodSubtitle.appendChild(foodCaption);
+        foodContent.appendChild(foodSubtitle);
+        var foodTitle = document.createElement('h2');
+        foodTitle.className = 'food-title';
+        foodTitle.innerText = food.Name;
+        foodContent.appendChild(foodTitle);
+
+        var foodDescription = document.createElement('p');
+        foodDescription.className = 'food-description';
+        foodDescription.innerText = food.HostWords;
+        foodContent.appendChild(foodDescription);
+
+        foodItem.appendChild(foodImage);
+        foodItem.appendChild(foodContent);
+
+        foods.appendChild(foodItem);
+      }
     } else {
       for (var i = 0; i < data.length; i++) {
         var food = data[i];
         var foodItem = document.createElement('div');
-        foodItem.className = mode === 'grid' ? 'food food-grid' : 'food food-list';
+        foodItem.className = 'food food-list';
 
         var foodImage = document.createElement('div');
         foodImage.className = 'food-img';
@@ -255,7 +302,7 @@
         foodItem.appendChild(foodImage);
         foodItem.appendChild(foodContent);
 
-        // foods.appendChild(foodItem);
+        foods.appendChild(foodItem);
       }
     }
   }
